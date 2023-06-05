e874dc6b1f75c87ed09884e2ec598a4248dbcb0b
Rejman <rejmans00@gmail.com>
2023-06-05 18:58:06 -0300

    Estrutura do Frond-end do Jogo

---
+++ criar-tabuleiro.js
@@ -0,0 +1,21 @@
+const body = document.querySelector("body");
+
+function criarTabuleiro() {
+  const div = document.createElement("div");
+
+  const board = div.cloneNode(true);
+  board.classList.add("board");
+  body.appendChild(board);
+
+  const cell = div.cloneNode(true);
+  cell.classList.add("cell");
+
+  const quadrados = Array(9).fill(0);
+  quadrados.forEach((_, index) => {
+    let identificador = index + 1;
+    cell.setAttribute("id", identificador);
+    board.appendChild(cell.cloneNode(true));
+  });
+}
+
+criarTabuleiro();
